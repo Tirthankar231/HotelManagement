15e92f2a1e84980e079992ea50fc6eace4bcca0f
Tirthankar007 <roytirthankar2@gmail.com>
2023-10-30 19:15:06 +0530

    add, added the room route

---
+++ routes/room.routes.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import roomController from '../controllers/room.controller.js';
+import { authenticateToken } from '../middlewares/authentication.js';
+import { authorizeUser, authorizeAdmin } from '../middlewares/authorisation.js'
+
+const router = new Router();
+
+// Create a new room
+router.post('/createRooms', authenticateToken, authorizeAdmin, roomController.create);
+
+// Get a specific room by ID
+router.get('/getRoomsById/:id', authenticateToken, authorizeUser, roomController.getById);
+
+// Update a specific room by ID
+router.put('/updateRooms/:id', authenticateToken, authorizeAdmin, roomController.update);
+
+// Delete a specific room by ID
+router.delete('/deleteRooms/:id', authenticateToken, authorizeAdmin, roomController.delete);
+
+// Get a list of rooms
+router.get('/getAllRooms', authenticateToken, roomController.list);
+
+export default router;
