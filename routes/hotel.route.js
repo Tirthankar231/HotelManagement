b41e1ed5ef1c33fa35e8fe0019ee6e4e4fdc2d18
Tirthankar007 <roytirthankar2@gmail.com>
2023-10-30 19:13:32 +0530

    add, added the hotel route

---
+++ routes/hotel.route.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import hotelController from '../controllers/hotel.controller.js';
+import { authenticateToken } from '../middlewares/authentication.js';
+import { authorizeUser, authorizeAdmin } from '../middlewares/authorisation.js'
+
+const router = new Router();
+
+// Create a new hotel
+router.post('/hotels', authenticateToken, authorizeAdmin, hotelController.create);
+
+// Get a specific hotel by ID
+router.get('/getHotelsById/:id', authenticateToken, authorizeUser, hotelController.getById);
+
+// Update a specific hotel by ID
+router.put('/updateHotels/:id', authenticateToken, authorizeAdmin, hotelController.update);
+
+// Delete a specific hotel by ID
+router.delete('/deleteHotels/:id', authenticateToken, authorizeAdmin, hotelController.delete);
+
+// Get a list of hotels
+router.get('/getAllHotels', authenticateToken, hotelController.list);
+
+export default router;
